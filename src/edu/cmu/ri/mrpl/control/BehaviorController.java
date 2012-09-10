d24d9063a30ac76b0995abb29e28b9c0acbdc1c6
Gabriel Garcia <ggarcia@andrew.cmu.edu>
2012-09-10 15:46:26 -0400

    Added bare-bone code for Behavior, just sets target point to (0,2).

--- src/edu/cmu/ri/mrpl/control/BehaviorController.java
+++ src/edu/cmu/ri/mrpl/control/BehaviorController.java
@@ -5,13 +5,25 @@ import fj.data.List;
 
 public class BehaviorController {
 
+	private RealPoint2D targetPoint;
+	
 	public BehaviorController(){
 		
+		setTarget(new RealPoint2D(0,0)); 
 	}
+	
 	public void updateBehavior(List<RealPoint2D> l){
+		//Look at list find unchecked area, go there
 		
+		//For now just set target in front of us
+		setTarget(new RealPoint2D(0,2));
 	}
-	public RealPoint2D getTargetPoint(){
-		return null;
+	
+	public RealPoint2D getTarget() {
+		return targetPoint;
+	}
+
+	public void setTarget(RealPoint2D target) {
+		this.targetPoint = target;
 	}
 }
