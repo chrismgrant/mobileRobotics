92421e133e5d9512e57631f00d3747af2180d179
Heli Wang <heli.h101@gmail.com>
2012-09-10 10:38:25 -0400

    Added BehaviorController implementation

---
+++ src/edu/cmu/ri/mrpl/control/BehaviorController.java
@@ -0,0 +1,17 @@
+package edu.cmu.ri.mrpl.control;
+
+import edu.cmu.ri.mrpl.kinematics2D.RealPoint2D;
+import fj.data.List;
+
+public class BehaviorController {
+
+	public BehaviorController(){
+		
+	}
+	public void updateBehavior(List<RealPoint2D> l){
+		
+	}
+	public RealPoint2D getTargetPoint(){
+		return null;
+	}
+}
