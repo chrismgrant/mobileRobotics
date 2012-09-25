1de37c48eb1c5d4aad5ef7fd49f09d0b40386c7d
Chris Grant <cgsofly@gmail.com>
2012-09-25 00:27:27 -0400

    BehaviorTester

---
+++ src/edu/cmu/ri/mrpl/control/BehaviorTester.java
@@ -0,0 +1,19 @@
+package edu.cmu.ri.mrpl.control;
+
+import java.awt.geom.Line2D;
+import java.util.ArrayList;
+
+import edu.cmu.ri.mrpl.Path;
+import edu.cmu.ri.mrpl.kinematics2D.LineSegment;
+import edu.cmu.ri.mrpl.kinematics2D.RealPoint2D;
+import edu.cmu.ri.mrpl.kinematics2D.RealPose2D;
+import edu.cmu.ri.mrpl.kinematics2D.Vector2D;
+import fj.F2;
+import fj.data.List;
+import fj.F;
+
+public class BehaviorTester {
+	Path dummyPath = new Path();
+	BehaviorController b = new BehaviorController();
+	
+}
