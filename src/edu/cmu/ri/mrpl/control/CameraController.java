88fa1dd1c6ced19353f005630240a8db4328fb37
Gabriel Garcia <ggarcia@andrew.cmu.edu>
2012-11-05 21:10:17 -0500

    pushed down JFrame in CamController

--- src/edu/cmu/ri/mrpl/control/CameraController.java
+++ src/edu/cmu/ri/mrpl/control/CameraController.java
@@ -3,7 +3,8 @@ package edu.cmu.ri.mrpl.control;
 import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
-
+import javax.swing.*;
+import java.awt.*;
 import edu.cmu.ri.mrpl.usbCamera.PicCanvas;
 import edu.cmu.ri.mrpl.usbCamera.UsbCamera;
 
@@ -14,47 +15,53 @@ import edu.cmu.ri.mrpl.usbCamera.UsbCamera;
  * Time: 5:41 PM
  * To change this template use File | Settings | File Templates.
  */
-public class CameraController extends JFrame {
-	private static final long serialVersionUID = 100L;
+public class CameraController {
 
-	private UsbCamera cam;
-	private PicCanvas canvas;
 	
-    public CameraController() {
+    public class CameraDriver extends JFrame {
         //TODO initialize webcam here
-		cam = UsbCamera.getInstance();
-		canvas = new PicCanvas();
-		getContentPane().add(canvas, BorderLayout.CENTER);
-		this.setSize(UsbCamera.XSIZE, UsbCamera.YSIZE);
+    	private static final long serialVersionUID = 100L;
 
-		/* Display the window */
-		this.setVisible(true);
+    	public UsbCamera cam;
+    	public PicCanvas canvas;
+    	
+    	public CameraDriver(){
+    		cam = UsbCamera.getInstance();
+			canvas = new PicCanvas();
+			getContentPane().add(canvas, BorderLayout.CENTER);
+			this.setSize(UsbCamera.XSIZE, UsbCamera.YSIZE);
 
+			/* Display the window */
+			this.setVisible(true);
+    	}
+    	
+    	public void update(){
+        	cam.snap();
+    		canvas.setImage(cam.getImage());
+
+        }
+        
+        public void printColors(int x, int y, int width, int height){
+        	canvas.setRect(x, y, width, height);
+        	for(int i=x; i<x+width; i++) {
+    			for(int j=y;j<y+height; j++) {
+    				try {
+    					cam.getRawPixel(i,j);
+    				} catch (Exception e) {
+    					System.out.println(e+" at " + i + "," + j);
+    				}
+    				int pixel[] = cam.getPixel(i, j);
+    				System.out.print(" r:"+pixel[UsbCamera.RED]+
+    						"g:"+pixel[UsbCamera.GREEN]+
+    						"b:"+pixel[UsbCamera.BLUE]+" ; ");
+    			}
+    			System.out.println();
+    		}
+        }
+        
     }
 
-    public void update(){
-    	cam.snap();
-		canvas.setImage(cam.getImage());
-
-    }
     
-    public void printColors(int x, int y, int width, int height){
-    	canvas.setRect(x, y, width, height);
-    	for(int i=x; i<x+width; i++) {
-			for(int j=y;j<y+height; j++) {
-				try {
-					cam.getRawPixel(i,j);
-				} catch (Exception e) {
-					System.out.println(e+" at " + i + "," + j);
-				}
-				int pixel[] = cam.getPixel(i, j);
-				System.out.print(" r:"+pixel[UsbCamera.RED]+
-						"g:"+pixel[UsbCamera.GREEN]+
-						"b:"+pixel[UsbCamera.BLUE]+" ; ");
-			}
-			System.out.println();
-		}
-    }
     /**
      * Checks the camera to see if robot is holding gold
      * @return whether robot is holding gold
